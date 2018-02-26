304cd86929055d4992ab3f9cbebd0015f59d6ea1
Alex Bliznecov <sc2tvsibiya@gmail.com>
2018-02-26 17:33:10 +0300

    v.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const input = document.getElementById('i');
-input.maxLength = 18;
+const maxLength = 18;
 
 /**
  * @param {KeyboardEvent} event 
@@ -8,38 +8,44 @@ let onKeyboardHandler = (event) => {
     /**
      * @type {string}
      */
-    let value = input.value;
-    let valueReplaced = value.replace(/\D/g, '');
-    let valueReplacedLength = valueReplaced.length;
-    let cursorPosition;
+    const valueNormalized = input.value.replace(/\D/g, '');
+    const valueNormalizedLength = valueNormalized.length;
+    let indexLastCharacter;
+
+    if (input.value.length > maxLength) {
+        input.value =  input.value.replace(/\d$/, '');
+        return
+    }
 
     switch(true) {
         
-        case  valueReplacedLength > 9:
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '+7 ($1) $2-$3-$4')
+        case  valueNormalizedLength > 9:
+        input.value = valueNormalized.replace(/7(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '+7 ($1) $2-$3-$4')
         break;
         
-        case valueReplacedLength > 7:
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})(\d{0,3})(\d{0,2})/, '+7 ($1) $2-$3-_')
+        case valueNormalizedLength > 7:
+        input.value = valueNormalized.replace(/7(\d{0,3})(\d{0,3})(\d{0,2})/, '+7 ($1) $2-$3-_')
         break;
         
-        case valueReplacedLength > 4:
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})(\d{0,3})/, '+7 ($1) $2-__-_')
+        case valueNormalizedLength > 4:
+        input.value = valueNormalized.replace(/7(\d{0,3})(\d{0,3})/, '+7 ($1) $2-__-_')
         break;
         
-        case valueReplacedLength > 1: 
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})/, '+7 ($1) ___-__-_')
+        case valueNormalizedLength > 1: 
+        input.value = valueNormalized.replace(/7(\d{0,3})/, '+7 ($1) ___-__-_');
+
         break;
         
-        case valueReplacedLength == 1 && valueReplaced.search(/([7-8])/) !== -1:
-        valueReplaced = valueReplaced.replace(/([7-8])/,'+7 (');
+        case valueNormalizedLength == 1 && valueNormalized.search(/([7-8])/) !== -1:
+        input.value = valueNormalized.replace(/([7-8])/,'+7 (');
         break;
         
-        case valueReplacedLength == 1:
-        valueReplaced = valueReplaced.replace(/(\d)/, '+7 ($1__) ___-__-__')
+        case valueNormalizedLength == 1:
+        input.value = valueNormalized.replace(/(\d)/, '+7 ($1) ___-__-__');
+        break;
     }
-    console.log(valueReplaced);
-    input.setSelectionRange(value.length +1, value.length +1);
-    input.value = valueReplaced;
+    
+    indexLastCharacter = input.value.lastIndexOf(valueNormalized.slice(-1))
+    input.setSelectionRange(indexLastCharacter + 1, indexLastCharacter + 1);
 }
 input.addEventListener('input', onKeyboardHandler)
