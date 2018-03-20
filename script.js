cb3125fb7bd66f96efcd0d5eddd7aa018a523785
sibiya666 <sc2tvsibiya@gmail.com>
2018-03-20 22:26:57 +0300

    v.0.2

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
 const input = document.getElementById('i');
-input.maxLength = 18;
+const keyCode = {
+    backspace: 8,
+    delete: 46
+};
+let isDelete = false;
+input.focus();
 
 /**
+ * 
+ * @param {string} mask
+ * @param {itemPosition} number
+ * @param {valueNormalized} string
+ * @return {string}
+ */
+const getValueReplace = (mask, itemPosition, valueNormalized, event) => {
+    const tempValue = mask.replace(/./g, (item) => {
+        if (/[_\d]/.test(item) &&  itemPosition < valueNormalized.length) {
+            return valueNormalized[ itemPosition++]
+        } else if ( itemPosition >= valueNormalized.length) {
+            return ''
+        } else {
+            return item
+        }
+    })
+    getCursorPosition(tempValue.length);
+    return tempValue + mask.slice(tempValue.length);
+}
+
+/**
+ * 
+ * @param {string} value 
+ * @param {string} maskNormalized 
+ * @return {string}
+ */
+const getValueNormalized = (value, maskNormalized) => {
+    let tempValue = value.replace(/\D/g, '');
+
+    if (maskNormalized.length >= tempValue.length) {
+        tempValue = /([7-8])/.test(tempValue[0]) ? maskNormalized  : maskNormalized + tempValue
+    }
+
+    return tempValue
+}
+
+/**
+ * 
+ * @param {number} valueReplacedLength 
+ */
+const getCursorPosition = (valueReplacedLength) => {
+    cursorPosition = isDelete ? input.selectionStart : valueReplacedLength;
+}
+
+/**
+ * 
  * @param {KeyboardEvent} event 
  */ 
-let onKeyboardHandler = (event) => {
-    /**
-     * @type {string}
-     */
-    let value = input.value;
-    let valueReplaced = value.replace(/\D/g, '');
-    let valueReplacedLength = valueReplaced.length;
-    let cursorPosition;
-
-    switch(true) {
-        
-        case  valueReplacedLength > 9:
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/, '+7 ($1) $2-$3-$4')
-        break;
-        
-        case valueReplacedLength > 7:
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})(\d{0,3})(\d{0,2})/, '+7 ($1) $2-$3-_')
-        break;
-        
-        case valueReplacedLength > 4:
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})(\d{0,3})/, '+7 ($1) $2-__-_')
-        break;
-        
-        case valueReplacedLength > 1: 
-        valueReplaced = valueReplaced.replace(/7(\d{0,3})/, '+7 ($1) ___-__-_')
-        break;
-        
-        case valueReplacedLength == 1 && valueReplaced.search(/([7-8])/) !== -1:
-        valueReplaced = valueReplaced.replace(/([7-8])/,'+7 (');
-        break;
-        
-        case valueReplacedLength == 1:
-        valueReplaced = valueReplaced.replace(/(\d)/, '+7 ($1__) ___-__-__')
-    }
-    console.log(valueReplaced);
-    input.setSelectionRange(value.length +1, value.length +1);
+const onKeyboardHandler = (event) => {
+    let itemPosition = 0;
+    const value = input.value;
+    const mask = `+7 (___) ___-__-__`;
+    const maskNormalized = mask.replace(/\D/g, '');
+    const valueNormalized = getValueNormalized(value, maskNormalized);
+    const valueReplaced = getValueReplace(mask, itemPosition, valueNormalized, event);
     input.value = valueReplaced;
+    input.setSelectionRange(cursorPosition, cursorPosition);
 }
+
 input.addEventListener('input', onKeyboardHandler)
+input.addEventListener('keydown', (event) => {
+    isDelete = ((keyCode.backspace || keyCode.delete) == event.keyCode);
+})
